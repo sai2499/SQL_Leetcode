ec491be67641981f4f7e929b62ee507e0d39b999
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-03-15 01:31:11 -0700

    Time: 779 ms (71.16%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-select distinct player_id, 
-Min(event_date) over (partition by player_id) as first_login
+select player_id, 
+Min(event_date) as first_login
 from Activity
-group by player_id, event_date
+group by player_id
