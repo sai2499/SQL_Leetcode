10c0592f8d8ce53094fd75738d543818c0644c2f
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-03-16 00:49:45 -0700

    Time: 662 ms (15.25%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 with Cte as (
-    Select question_id as survey_log, count(*) as c
+    Select question_id, count(*) as c
     from SurveyLog
     where action = "answer"
-    group by survey_log
+    group by question_id
 )
 
-select survey_log
+select question_id as survey_log
 from Cte
 order by c desc ,survey_log asc
 limit 1
