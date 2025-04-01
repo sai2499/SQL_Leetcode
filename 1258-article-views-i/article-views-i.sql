8237789ba8aaa035abdaae9551b02aad26f1333e
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-01 01:22:28 -0700

    Time: 412 ms (93.74%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
-with cte as(
-    select *
-    from Views
-    where author_id = viewer_id
-)
+-- with cte as(
+--     select *
+--     from Views
+--     where author_id = viewer_id
+-- )
 
 select distinct author_id as id
-from cte order by author_id;
+from 
+(
+    select author_id
+    from Views
+    where author_id = viewer_id 
+) 
+    as t order by author_id;
