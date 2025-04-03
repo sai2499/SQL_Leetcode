c8e98cd475775deedd6c277c879398964d91a88e
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-03 00:42:37 -0700

    Time: 817 ms (88.39%) | Memory: 0B (100.00%) - LeetSync

--- 1318-tournament-winners/tournament-winners.sql
+++ 1318-tournament-winners/tournament-winners.sql
@@ -1,26 +1,23 @@
 # Write your MySQL query statement below
 with cte1 as(
-select distinct match_id, second_player as player, second_score as score
-from Matches
-union
-select distinct match_id, first_player as player, first_score as score
-from Matches),
+    select distinct match_id, second_player as player, second_score as score
+    from Matches
+    union all
+    select distinct match_id, first_player as player, first_score as score
+    from Matches),
 cte2 as(
-    select distinct player as player_id, sum(score) as total_score
+    select distinct player, sum(score) as total_score
     from cte1
     group by player
+),
+cte3 as(
+    select p.player_id, p.group_id, c.total_score, rank() over (partition by group_id order by total_score desc, player_id) as rank_val
+    from cte2 c
+    left join Players p
+    on c.player = p.player_id
 )
 
-SELECT 
-    DISTINCT group_id, 
-    FIRST_VALUE(cte2.player_id) OVER (
-    PARTITION BY group_id 
-    ORDER BY 
-      total_score DESC, 
-      cte2.player_id
-  ) AS player_id
-FROM 
-  cte2
-  LEFT JOIN players ON cte2.player_id = players.player_id
-
+select group_id as GROUP_ID, player_id as PLAYER_ID
+from cte3
+where rank_val = 1
 
