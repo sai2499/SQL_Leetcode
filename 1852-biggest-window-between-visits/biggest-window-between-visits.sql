0d3f83c7d2b3f66d8673f11eee4763eb13ab29dc
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-12 23:24:50 -0700

    Time: 406 ms (52.79%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -3,10 +3,9 @@ with cte as (
     select user_id, 
         visit_date,
         (coalesce(LEAD(visit_date) over (partition by user_id order by visit_date), '2021-1-1') - visit_date) as diff
-    from UserVisits),
-cte2 as (
-    select *, rank() over (partition by user_id order by diff desc) as rnk
-    from cte
-)
+    from UserVisits)
 
-select distinct user_id, diff as biggest_window from cte2 where rnk =1
+select user_id, 
+    max(diff) as biggest_window 
+from cte
+group by user_id
