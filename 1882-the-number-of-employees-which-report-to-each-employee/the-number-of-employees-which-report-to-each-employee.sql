6e853c2027c73ab900b719df0ae3523e486680ae
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-13 01:06:59 -0700

    Time: 346 ms (77.75%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,15 +1,12 @@
 -- Write your PostgreSQL query statement below
-with cte as (
-    select *
-    from Employees
-    where reports_to is not null
-),cte2 as(
+with cte as(
 select distinct *, count(reports_to) over (partition by reports_to) as emp_coount, 
 round(avg(age) over (partition by reports_to)) as emp_age
-from cte)
+from Employees
+where reports_to is not null)
 
 select distinct e.employee_id, e.name, c.emp_coount as reports_count, c.emp_age as average_age
 from Employees e
-join cte2 c
+join cte c
 on e.employee_id = c.reports_to
 order by employee_id
