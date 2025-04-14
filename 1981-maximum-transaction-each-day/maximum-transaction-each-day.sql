f1ba62a3a71e0f07d1cc0b2201d009d72238b215
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-13 21:09:56 -0700

    Time: 288 ms (74.14%) | Memory: 0B (100.00%) - LeetSync

--- 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
@@ -1,9 +1,10 @@
 -- Write your PostgreSQL query statement below
-with cte as(
-select *, rank() over (partition by TO_CHAR(day, 'dd/mm/yyyy') order by amount desc) as rnk
-from Transactions)
+-- with cte as(
+-- select *, rank() over (partition by TO_CHAR(day, 'dd/mm/yyyy') order by amount desc) as rnk
+-- from Transactions)
 
 select transaction_id 
-from cte 
+from (select *, rank() over (partition by TO_CHAR(day, 'dd/mm/yyyy') order by amount desc) as rnk
+from Transactions)
 where rnk = 1
 order by transaction_id
