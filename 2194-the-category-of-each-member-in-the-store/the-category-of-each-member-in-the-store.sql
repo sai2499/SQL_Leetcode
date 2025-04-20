36227aa9581f734561aab63ff6b0bcc034ade952
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-20 11:41:01 -0700

    Time: 371 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2194-the-category-of-each-member-in-the-store/the-category-of-each-member-in-the-store.sql
@@ -0,0 +1,19 @@
+-- select member_id, name, coalesce(((100*sum(charged_amount))/count(visit_date)),0) as conversion_rate
+with cte as (
+    select v.member_id, coalesce(((100*count(p.charged_amount))/count(v.visit_date)),0) as conversion_rate
+    from Visits v
+    left join Purchases p
+    using (visit_id)
+    group by member_id)
+
+
+select m.member_id, m.name,
+    CASE
+        WHEN c.conversion_rate >= 80 THEN 'Diamond'
+        WHEN c.conversion_rate BETWEEN 50 AND 80 THEN 'Gold'
+        WHEN c.conversion_rate < 50 THEN 'Silver'
+        WHEN c.conversion_rate IS NULL THEN 'Bronze'
+    END AS category
+from cte c
+right join Members m
+using (member_id)
