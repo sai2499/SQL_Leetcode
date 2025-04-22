3c6567e69ec12cb7385cef09f373d98ae31b1319
JAGATH SAI NARAYANA KAKARAPARTY <32254075+sai2499@users.noreply.github.com>
2025-04-21 22:37:14 -0700

    Time: 582 ms (12.76%) | Memory: 0B (100.00%) - LeetSync

--- 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -1,14 +1,7 @@
 -- Write your PostgreSQL query statement below
-with cte as (
-    select *, rank() over (order by points desc, name)
-    from TeamPoints
-),
-cte2 as (
-    select c.team_id, c.name, c.rank, rank() over (order by c.points+p.points_change desc, c.name) as new_rank
-    from cte c
-    join PointsChange p
-    using (team_id)
-)
-
-select team_id, name, rank-new_rank as rank_diff
-from cte2
+select team_id, 
+name, 
+rank() over (order by points desc, name) - rank() over (order by c.points+p.points_change desc, c.name) as rank_diff
+from TeamPoints c
+join PointsChange p
+using (team_id)
